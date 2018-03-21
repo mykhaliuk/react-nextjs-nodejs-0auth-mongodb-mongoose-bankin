723ff6cf06b6eca00981600884ce59c6116e2dbe
Mykhaliuk Volodymyr <mykhaliuk@me.com>
2018-03-21 11:55:38 +0100

    init

---
+++ server/app.js
@@ -0,0 +1,54 @@
+import express           from 'express'
+import session           from 'express-session'
+import mongoSessionStore from 'connect-mongo'
+import next              from 'next'
+import mongoose          from 'mongoose'
+import getRootUrl        from '../lib/api/getRootUrl'
+import auth              from './google'
+
+require('dotenv').config()
+
+const dev = process.env.NODE_ENV !== 'production'
+const MONGO_URL = process.env.MONGO_URL_TEST
+
+mongoose.connect(MONGO_URL)
+
+const port = process.env.PORT || 8000
+const ROOT_URL = getRootUrl()
+
+const app = next({dev})
+const handle = app.getRequestHandler()
+
+// Nextjs's server prepared
+app.prepare().then(() => {
+  const server = express()
+
+  // confuring MongoDB session store
+  const MongoStore = mongoSessionStore(session)
+  const sess = {
+    name             : 'bb.sid',
+    secret           : 'HD2w.)q*VqRT4/#NK2M/,E^B)}FED5fWU!dKe[wk',
+    store            : new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl               : 14 * 24 * 60 * 60 // save session 14 days
+    }),
+    resave           : false,
+    saveUninitialized: false,
+    cookie           : {
+      httpOnly: true,
+      maxAge  : 14 * 24 * 60 * 60 * 1000
+    }
+  }
+
+  server.use(session(sess))
+
+  auth({server, ROOT_URL})
+
+  server.get('*', (req, res) => handle(req, res))
+
+  // starting express server
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log('✨  Magic happens on ' + ROOT_URL + ' [' + process.env.NODE_ENV + ']  ✨') // eslint-disable-line no-console
+  })
+})
