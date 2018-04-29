e6e4d8c1ad5559c72e8df615d07513b6671387cb
Mykhaliuk Volodymyr <mykhaliuk@me.com>
2018-04-29 19:21:31 +0200

    Added FastClick https://github.com/ftlabs/fastclick

--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,7 @@ class Index extends React.Component {
         <Head>
           <title>Dashboard</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <script src="https://as.alipayobjects.com/g/component/fastclick/1.0.6/fastclick.js"></script>
           <meta name="apple-touch-fullscreen" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
